ee0556f3cd52ab69c24c35ca1cd6fc465cce437e
孙红祥 <v-sunhongxiang@ainnovation.com>
2020-10-14 11:27:06 +0800

    <feat>(db): save info to db

---
+++ db/mysql/conn.go
@@ -0,0 +1,21 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+var DB *sql.DB
+
+func init() {
+	db, _ := sql.Open("mysql", "root:qwer1234@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	db.SetMaxOpenConns(1000)
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("failed to connect mysql, err:", err.Error())
+		os.Exit(1)
+	}
+	DB = db
+}
